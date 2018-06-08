0f512e92ba13e6f816eb504f2b9190c7f1959f84
Dior Huang <hehuang@cimpress.com>
2018-06-08 09:28:34 +0800

    Added list customers & movies excercises

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private List<Customer> customers = new List<Customer>
+        {
+            new Customer {Id = 1, Name = "Dior"},
+            new Customer {Id = 2, Name = "Kaden"},
+            new Customer {Id = 3, Name = "Jerry"},
+        };
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View(customers);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var detail = customers.Find(c => c.Id == id);
+            return View(detail);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
@@ -13,8 +14,50 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!"};
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer 1"},
+                new Customer {Name = "Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            //return new ViewResult();
+            //ViewData["Movie"] = movie;
 
-            return View(movie);
+            return View(viewModel);
+            //return RedirectToAction("Index", "Home", new { page=1, sortBy = "name"});
+        }
+
+        public ActionResult Edit(int moveiId)
+        {
+            return Content("id=" + moveiId);
+        }
+
+        // movies
+        //public ActionResult Index(int? pageIndex, string sortBy)
+        public ActionResult Index()
+        {
+//            if (!pageIndex.HasValue)
+//                pageIndex = 1;
+//            if (string.IsNullOrEmpty(sortBy))
+//                sortBy = "Name";
+//            return Content(string.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            var movies = new List<Movie>
+            {
+                //new Movie { Id = 1, Name = "Revengers"},
+                //new Movie { Id = 2, Name = "Spider"},
+            };
+
+            return View(movies);
+        }
+
+        [Route("movies/released/{year:regex(\\d{4})}/{month:range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
