7e7122fdd7d3ef7315ded3dc8c93484928be4f87
Unknown <huangstar9@126.com>
2018-06-21 21:26:23 +0800

    Part of excercise

---
+++ Vidly/Controllers/Api/RentalsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateRental(RentalDto rentalDto)
+        {
+
+            throw new NotImplementedException();
+
+        }
+
+    }
+}
---
+++ Vidly/Models/Rental.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public Movie Movie { get; set; }
+        public DateTime DateRented { get; set; }
+        public DateTime? DateReturned { get; set; }
+
+    }
+}
